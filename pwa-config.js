f64aacf042dfc7dd61c41550bc7957f16b25425f
César Alexandre <cesarcosta16@gmail.com>
2025-09-23 21:33:26 +0000

    quando fico offline nao funciona, so aparece a logo e escrito q estou of

--- pwa-config.js
+++ pwa-config.js
@@ -1,6 +1,9 @@
+
+const runtimeCaching = require('next-pwa/cache');
+
 module.exports = {
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  runtimeCaching,
 };
